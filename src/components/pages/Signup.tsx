71c8b2734569ff094dae074602256241f19c4be9
CrestNiraj12 <crestniraj@gmail.com>
2022-11-03 19:01:11 +0545

    made more responsive

--- src/components/pages/Signup.tsx
+++ src/components/pages/Signup.tsx
@@ -5,7 +5,7 @@ import SignupForm from "../UI/organisms/SignupForm";
 function App() {
   return (
     <main className="flex">
-      <div className="w-full lg:w-[22%] flex flex-col justify-center items-center mx-20 mt-28 mb-20 text-center">
+      <div className="w-full lg:w-[22%] flex flex-col justify-center items-center mx-5 sm:mx-20 mt-28 mb-20 text-center">
         <Header />
         <SignupForm />
       </div>
